527246eb1a5df8ce5c10e1f62ba9d4f06fec6855
rahulsainlll <rahulsain.social777@gmail.com>
2024-05-20 23:49:17 +0530

    two sum

--- Neetcode 150/Arrays/Two_Sum.cpp
+++ Neetcode 150/Arrays/Two_Sum.cpp
@@ -1,2 +1,18 @@
-https://leetcode.com/problems/two-sum/description/
+// https://leetcode.com/problems/two-sum/description/
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+
+        for (int i = 0 ; i < n ; i++) {
+            int c  = target - nums[i];
+            if(mp.find(c) !=mp.end()){
+                return {mp[c], i};
+            }
+            mp.insert({nums[i], i});
+        }
+        return {};
+    }
+};
