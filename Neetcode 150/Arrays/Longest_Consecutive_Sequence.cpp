b3850ba79da258f10616e736aea11a94528fd9ba
rahulsainlll <rahulsain.social777@gmail.com>
2024-05-26 23:22:00 +0530

    dsa

--- Neetcode 150/Arrays/Longest_Consecutive_Sequence.cpp
+++ Neetcode 150/Arrays/Longest_Consecutive_Sequence.cpp
@@ -1,2 +1,20 @@
 // https://leetcode.com/problems/longest-consecutive-sequence/description/
 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int n : numSet) {
+            if (numSet.find(n - 1) == numSet.end()) {
+                int length = 1;
+                while (t) {
+                    length++;
+                }
+                longest = max(length, longest);
+            }
+        }
+        return longest;
+    }
+};
